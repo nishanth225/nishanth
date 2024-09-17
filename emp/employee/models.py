cc40bcf21aa2e14cf6268839abe1bf7950ee033d
nishanth225 <nishunishanth713@gmail.com>
2024-09-17 08:46:13 +0530

    first commit

---
+++ emp/employee/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class employee (models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+
+
+
+# Create your models here.
